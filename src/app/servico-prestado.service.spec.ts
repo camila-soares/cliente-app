40e69ad8c2f786051269cd284a596e4fc4953f72
camilasoares <camilasoares1507@gmail.com>
2021-03-03 12:22:14 -0300

    all

--- src/app/servico-prestado.service.spec.ts
+++ src/app/servico-prestado.service.spec.ts
@@ -1,12 +1,34 @@
+import { ServicoPrestadoBusca } from './servico-prestado/ServicoPrestadoBusca';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ServicoPrestadoService } from './servico-prestado.service';
 
-describe('ServicoPrestadoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ServicoPrestadoService', () => {
+  let service: ServicoPrestadoService;
+  let httpTestingController: HttpTestingController;
 
-  it('should be created', () => {
-    const service: ServicoPrestadoService = TestBed.get(ServicoPrestadoService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ServicoPrestadoService],
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.get(ServicoPrestadoService)
   });
+
+  it('should return a list of clients', () => {
+      const dummyResponse = {
+        ServicoPrestadoBusca: [{
+          id: 1,
+          description: 'Camila',
+          value: '100,00',
+          date: '03/03/2021'
+        }, {
+          id: 2,
+          description: 'Fulano',
+          value: '200,00',
+          date: '030332021'
+        }]
+      };
+     })
 });
